793de2394157300c2905fcc02703b5cfb47236c3
JONGWOOK2928 <a01029289121@gmail.com>
2026-05-28 14:06:51 +0900

    MemberController 구문추가

--- Know-How-Academy_Project/src/main/java/com/kh/know_how/member/controller/MemberController.java
+++ Know-How-Academy_Project/src/main/java/com/kh/know_how/member/controller/MemberController.java
@@ -1,11 +1,21 @@
 package com.kh.know_how.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.kh.know_how.member.model.service.MemberService;
 
 @Controller
 @RequestMapping("member")
 public class MemberController {
 
+	@GetMapping("/myPage")
+	public String myPage() {
+		
+	return "member/myPage";
+	}
 	 
 }
