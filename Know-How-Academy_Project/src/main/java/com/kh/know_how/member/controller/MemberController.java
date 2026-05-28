8333046062d582df29d715cab324ec2b264a6fde
JONGWOOK2928 <a01029289121@gmail.com>
2026-05-28 11:28:09 +0900

    user-> member 로 교체

---
+++ Know-How-Academy_Project/src/main/java/com/kh/know_how/member/controller/MemberController.java
@@ -0,0 +1,11 @@
+package com.kh.know_how.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("member")
+public class MemberController {
+
+	 
+}
---
+++ Know-How-Academy_Project/src/main/java/com/kh/know_how/member/model/service/MemberService.java
@@ -0,0 +1,5 @@
+package com.kh.know_how.member.model.service;
+
+public class MemberService {
+
+}
