548faf35b2e37502102f91a059fb6a9785d34526
JONGWOOK2928 <a01029289121@gmail.com>
2026-06-15 12:20:06 +0900

    2026 06 15 정종욱
    오류시 페이지이동 수정 비밀번호찾기 버튼 클릭 시 이메일로 번호 랜덤으로 변경후 발송 학생리스트 수정

--- Know-How-Academy_Project/src/main/java/com/kh/know_how/counselor/model/dao/CounselorDao.java
+++ Know-How-Academy_Project/src/main/java/com/kh/know_how/counselor/model/dao/CounselorDao.java
@@ -8,13 +8,15 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.kh.know_how.common.model.vo.ClassPageInfo;
+import com.kh.know_how.member.model.vo.Member;
+import com.kh.know_how.counselor.model.vo.Class;
 
 @Repository
 public class CounselorDao {
 
-	public ArrayList<Class> selectClassDetailList(SqlSessionTemplate sqlSession) {
+	public ArrayList<Class> selectClassDetailList(SqlSessionTemplate sqlSession, Member m) {
 		
-		return (ArrayList)sqlSession.selectList("counselorMapper.selectClassDetailList");
+		return (ArrayList)sqlSession.selectList("counselorMapper.selectClassDetailList", m);
 	}
 
 	
